2ec0cfcc1e90d834e86018416238ad2a9b982dc9
Partsaleva <m_partsaleva@abv.bg>
2016-05-28 21:53:51 +0300

    pract

--- Algorithm/src/threads/pi/CalculatePi.java
+++ Algorithm/src/threads/pi/CalculatePi.java
@@ -1,20 +1,19 @@
 package threads.pi;
 
-import java.math.BigDecimal;
-import java.util.Calendar;
-
-public class CalculatePi{
-	
-	public static void main(String[] args) {
-		int k=0;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+ 
+public class CalculatePi  {
+ 
+  
+     
+    public static void main(String args[]){
+    	int k=0;
 		int numberOfThreads = 0;
 		boolean quiet=false;
-		Thread[] array;
-		long sum = 0;
-		
-		int begin=0, end = 0;
-	
-		long timeOfStart = Calendar.getInstance().getTimeInMillis();
 		
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equals("-p")) {
@@ -32,53 +31,44 @@ public class CalculatePi{
 			System.out.println("ERROR: Args are not correct!!!");
 			
 		}else{
-			 array=new Thread[numberOfThreads];
-			 for (int i = 0; i < numberOfThreads; i++) {
-				begin=(i * k)/numberOfThreads;
-				end= k *(i+1)  / numberOfThreads;
-				
-				MyThread partSum=new MyThread("Thread " +i, begin, end);
-				Thread thread=new Thread(partSum);
-				thread.start();
-				array[i]=thread;
-				sum+=partSum.getSum();	
-				
-			}
-			
-			for (int i = 0; i < array.length; i++) {
-				try {
-					array[i].join();
-				} catch (InterruptedException e) {
+			long start=System.nanoTime();
+	        //Get ExecutorService from Executors utility class, thread pool size is 10
+	        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+	        //create a list to hold the Future object associated with Callable
+	        double result=0;
+	        //Create MyCallable instance
+	        Callable<Double> callable;
+	        for(int i=0; i < k; i++){
+	        	callable = new PartSum(i,quiet);
+	            //submit Callable tasks to be executed by thread pool
+	            Future<Double> future = executor.submit(callable);
+	           
+	            //add Future to the list, we can get return value using Future
+	            try {
+					result+=future.get();
+				} catch (InterruptedException | ExecutionException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+	        }
+	      
+	        System.out.println();
+	        System.out.println(1 / (((2 * Math.sqrt(2)) / 9801) * result));
+	        
+	        long time=System.nanoTime() - start;
+	        if (!quiet) {
+				printInfo(numberOfThreads, time);
 			}
-			 
-			 System.out.println(1 / (((2 * Math.sqrt(2)) / 9801) * sum));
-		}
+	        //shut down the executor service now
+	        executor.shutdown();
+	    }
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-	} 
-		
-
-
-	
-	
-	
-	public  double ramanujanSeries(long n) {
-		double sum = 0;
-		for(int k = 0; k < n; k++){
-			
-		}
-		return 1 / (((2 * Math.sqrt(2)) / 9801) * sum);
+    }
+    
+    public static void printInfo(int threads, long time){
+		System.out.println("Threads used in current run: " + threads);
+		System.out.println("Total execution time for current run (millis): "+time);
 	}
-
-	
+ 
 }
