5a74155fe8771572ded33979161b4c6fad7cb208
Partsaleva <m_partsaleva@abv.bg>
2016-07-02 08:28:07 +0300

    Added solution Problem 27

---
+++ Project Euler/src/Problem27.java
@@ -0,0 +1,51 @@
+/*Euler discovered the remarkable quadratic formula:
+
+n² + n + 41
+
+It turns out that the formula will produce 40 primes for the consecutive 
+values n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 
+is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+
+The incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes 
+for the consecutive values n = 0 to 79. The product of the coefficients, −79 and 1601, is −126479.
+
+Considering quadratics of the form:
+
+n² + an + b, where |a| < 1000 and |b| < 1000
+where |n| is the modulus/absolute value of n
+e.g. |11| = 11 and |−4| = 4
+Find the product of the coefficients, a and b, for the quadratic expression that produces 
+the maximum number of primes for consecutive values of n, starting with n = 0.*/
+public class Problem27 {
+
+    public static void main(String[] args) {
+        int a = 0,b =0, numberOfPrimes=0;
+        for (int i = -1000; i < 1000; i++) {
+            for (int j = -1000; j < 1000; j++) {
+                int n=0;
+                while(isPrime(Math.abs(n * n + i * n + j))){
+                    n++;
+                }
+                if (n>numberOfPrimes) {
+                    numberOfPrimes = n;
+                    a=i;
+                    b=j;
+                }
+            }
+        }
+        System.out.println(a*b);
+
+    }
+
+    
+    
+    public static boolean isPrime(int number){
+        int max=(int) Math.sqrt(number);
+        for (int i = 2; i < max; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
