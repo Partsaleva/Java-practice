58bbe1bfa8712ea81c6bea9cd69004430672b4e7
Partsaleva <m_partsaleva@abv.bg>
2016-08-19 07:51:18 +0300

    Homework1 advanced Java

---
+++ Softuni/Java Advanced/h1Java_Advanced_Stacks_and_Queues_Lab/OutputWriter.java
@@ -0,0 +1,19 @@
+package h1Java_Advanced_Stacks_and_Queues_Lab;
+
+public class OutputWriter {
+    public static void writeMessage(String message) {
+        System.out.print(message);
+    }
+    
+    public static void writeMessageOnNewLine(String message) {
+        System.out.println(message);
+    }
+    
+    public static void writeEmptyLine() {
+        System.out.println();
+    }
+    
+    public static void displayException(String message) {
+        System.out.print(message);
+    }  
+}
---
+++ Softuni/Java Advanced/h1Java_Advanced_Stacks_and_Queues_Lab/Problem3ImplementBFSAlg.java
@@ -0,0 +1,30 @@
+package h1Java_Advanced_Stacks_and_Queues_Lab;
+
+import java.io.File;
+import java.util.LinkedList;
+
+public class Problem3ImplementBFSAlg {
+
+    public static void main(String[] args) {
+        try {
+            traverseDirectory("/home/partsaleva/Documents");
+        } catch (Exception e) {
+            System.err.println("Access denied");
+        }
+    }
+    
+    public static void traverseDirectory(String path) {
+        LinkedList<File> subFolders=new LinkedList<>();
+        File root=new File(path);
+        subFolders.add(root);
+        while (subFolders.size()!=0) {
+            File curentFolder=subFolders.removeFirst();
+            for (File file : curentFolder.listFiles()) {
+                if (file.isDirectory()) {
+                    subFolders.add(file);
+                }
+            }
+            System.out.println(curentFolder.toString());
+        }
+    }
+}
