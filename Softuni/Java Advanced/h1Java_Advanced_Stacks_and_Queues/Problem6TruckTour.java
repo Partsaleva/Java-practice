5eabf5ba2e83578dc1b706c642ec12c131cbcb88
Partsaleva <m_partsaleva@abv.bg>
2016-08-27 13:09:51 +0300

    Added solution for truck tour problem

--- Softuni/Java Advanced/h1Java_Advanced_Stacks_and_Queues/Problem6TruckTour.java
+++ Softuni/Java Advanced/h1Java_Advanced_Stacks_and_Queues/Problem6TruckTour.java
@@ -7,30 +7,56 @@ import java.util.Scanner;
 public class Problem6TruckTour {
 
     public static void main(String[] args) {
-        int n=3;
-        int petrol=0;
-        int dist=0;
-        int amountOfPetrol=0;
-        int distances=0;
-        Queue<Integer> fuel=new LinkedList<>();
-        Queue<Integer> distance=new LinkedList<>();
-        Scanner scan=new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
-            String[] data=scan.nextLine().split(" ");
-            petrol=Integer.parseInt(data[0]);
-            dist=Integer.parseInt(data[1]);
-            amountOfPetrol+=petrol;
-            distances+=dist;
-            fuel.add(petrol); distance.add(dist);
-        }
-        if (distances>amountOfPetrol) {
-            System.out.println("Next time");
-        }else{
-            for (int i = 0; i < n; i++) {
-                
-            }
-        }
-        scan.close();
-    }
+        int n=5;
+        Queue<Pump> petrol=new LinkedList<>();
+//        Scanner scan=new Scanner(System.in);       
+//        for (int i = 0; i < n; i++) {
+//            String[] data=scan.nextLine().split(" ");
+//            petrol.add(new Pump(Integer.parseInt(data[0]),Integer.parseInt(data[1])));
+//        }       
+//        scan.close();
+       
 
+        petrol.add(new Pump(1, 5));
+        petrol.add(new Pump(10, 3));
+        petrol.add(new Pump(3, 4));
+        
+//        petrol.add(new Pump(4, 6));
+//        petrol.add(new Pump(6, 5));
+//        petrol.add(new Pump(7, 3));
+//        petrol.add(new Pump(4, 5));
+        
+        System.out.println(findCircle(petrol));
+    }
+    
+    public static int findCircle(Queue<Pump> petrol){
+        int index=1;    
+        int quantity=0;
+        while(!petrol.isEmpty()){
+            Pump p=petrol.poll();
+            if(!petrol.isEmpty()){
+                Pump p1=petrol.peek();
+                quantity=quantity+p.quantity;
+                    if(quantity>=p1.distance){
+                          quantity=quantity-p1.distance;
+                        continue;
+                    }
+                    else{
+                        index++;
+                        quantity=0;
+                        petrol.add(p);
+                    }
+            } else break;
+        }
+        return index;
+    }
+    
+    static class Pump{
+        int quantity;
+        int distance;
+        public Pump(int q,int d) {
+            this.quantity=q;
+            this.distance=d;
+        }
+    }
 }
