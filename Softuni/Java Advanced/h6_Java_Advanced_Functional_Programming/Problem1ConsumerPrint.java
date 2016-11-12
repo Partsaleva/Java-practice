8a0bb93531e7ecd176a6dab70a7428a4875b4db1
Partsaleva <m_partsaleva@abv.bg>
2016-11-12 09:34:29 +0200

    Added solution for Problem 1 Funcional Programming

---
+++ Softuni/Java Advanced/h6_Java_Advanced_Functional_Programming/Problem1ConsumerPrint.java
@@ -0,0 +1,19 @@
+package h6_Java_Advanced_Functional_Programming;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Problem1ConsumerPrint {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String[] input=scan.nextLine().split(" ");
+        Consumer<String> print = name -> System.out.println(name);
+        for (String name : input) {
+            print.accept(name);
+        }
+        scan.close();
+
+    }
+
+}
